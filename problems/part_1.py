eb94799d3949d92a6df4fee610a26f9780864082
ClinicerAnssi <anssi.wathen@successclinic.fi>
2024-05-27 12:56:12 +0300

    Initial commit

---
+++ problems/part_1.py
@@ -0,0 +1,79 @@
+
+
+# https://dmoj.ca/problem/wc16c1j1 - A Spooky Season
+
+# https://dmoj.ca/problem/wc15c2j1 - A New Hope
+
+# https://dmoj.ca/problem/ccc13j1 - Next in Line
+
+# https://dmoj.ca/problem/wc17c1j2 - How's the Weather?
+
+# https://dmoj.ca/problem/wc18c3j1 - An Honest Day's Work
+
+
+# https://dmoj.ca/problem/ccc06j1 - Canadian Calorie Counting
+# https://dmoj.ca/problem/ccc15j1 - Special Day
+# https://dmoj.ca/problem/ccc15j2 - Happy or Sad
+# https://dmoj.ca/problem/dmopc16c1p0 - C.C. and Cheese-kun
+# https://dmoj.ca/problem/ccc07j1 - Who is in the Middle
+
+# https://dmoj.ca/problem/wc17c3j3 - Uncrackable
+# https://dmoj.ca/problem/coci18c3p1 - Magnus
+# https://dmoj.ca/problem/ccc11s1 - English or French
+# https://dmoj.ca/problem/ccc11s2 - Multiple Choice
+# https://dmoj.ca/problem/coci12c5p1 - Ljestvica
+# https://dmoj.ca/problem/coci13c3p1 - Rijeci
+# https://dmoj.ca/problem/coci18c4p1 - Elder
+
+# https://dmoj.ca/problem/ccc20j2 - Epidemiology
+# https://dmoj.ca/problem/coci08c1p2 - Ptice
+# https://dmoj.ca/problem/ccc02j2 - AmeriCanadian
+# https://dmoj.ca/problem/ecoo13r1p1 - Take a Number
+# https://dmoj.ca/problem/ecoo15r1p1 - When You Eat Your Smarties
+# https://dmoj.ca/problem/ccc19j3 - Cold Compress
+
+# https://dmoj.ca/problem/ccc07j3 - Deal or No Deal Calculator
+# https://dmoj.ca/problem/coci17c1p1 - Cezar
+# https://dmoj.ca/problem/coci18c2p1 - Preokret
+# https://dmoj.ca/problem/ccc00s2 - Babbling Brooks
+# https://dmoj.ca/problem/ecoo18r1p1 - Willow's Wild Ride
+# https://dmoj.ca/problem/ecoo19r1p1 - Free Shirts
+# https://dmoj.ca/problem/dmopc14c7p2 - Tides
+# https://dmoj.ca/problem/wac3p3 - Wesley Plays DDR
+# https://dmoj.ca/problem/ecoo18r1p2 - Rue's Rings
+# https://dmoj.ca/problem/coci19c5p1 - Emacs
+# https://dmoj.ca/problem/coci20c2p1 - Crtanje
+# https://dmoj.ca/problem/dmopc19c5p2 - Charlie's Crazy Conquest
+
+# https://dmoj.ca/problem/ccc13s1 - From 1987 to 2013
+# https://dmoj.ca/problem/ccc18j3 - Are we there yet?
+# https://dmoj.ca/problem/ecoo12r1p2 - Decoding DNA
+# https://dmoj.ca/problem/crci07p1 - Platforme
+# https://dmoj.ca/problem/coci13c2p2 - Misa
+
+# http://www.usaco.org/index.php?page=viewproblem2&cpid=855 - Mixing Milk
+# http://www.usaco.org/index.php?page=viewproblem2&cpid=711 - Why Did the Cow Cross the Road
+# http://www.usaco.org/index.php?page=viewproblem2&cpid=735 - The Lost Cow
+# http://www.usaco.org/index.php?page=viewproblem2&cpid=963 - Cow Gymnastics
+# http://www.usaco.org/index.php?page=viewproblem2&cpid=736 - Bovine Genomics
+# http://www.usaco.org/index.php?page=viewproblem2&cpid=831 - Team Tic Tac Toe
+# http://www.usaco.org/index.php?page=viewproblem2&cpid=918 - Sleepy Cow Herding
+
+# https://dmoj.ca/problem/crci06p1 - Bard
+# https://dmoj.ca/problem/dmopc19c5p1 - Conspicuous Cryptic Checklist
+# https://dmoj.ca/problem/coci15c2p1 - Marko
+# https://dmoj.ca/problem/ccc06s2 - Attack of the CipherTexts
+# https://dmoj.ca/problem/dmopc19c3p1 - Mode Finding
+# https://dmoj.ca/problem/coci14c2p2 - Utrka
+# https://dmoj.ca/problem/coci17c2p2 - ZigZag
+
+# http://www.usaco.org/index.php?page=viewproblem2&cpid=891 - Shell Game
+# http://www.usaco.org/index.php?page=viewproblem2&cpid=639 - Diamond Collector
+# https://dmoj.ca/problem/coci20c1p1 - Patkice
+# https://dmoj.ca/problem/ccc09j2 - Old Fishin' Hole
+# https://dmoj.ca/problem/ecoo16r1p2 - Spindie
+# https://dmoj.ca/problem/cco96p2 - SafeBreaker
+# http://www.usaco.org/index.php?page=viewproblem2&cpid=964 - Where Am I
+# http://www.usaco.org/index.php?page=viewproblem2&cpid=592 - Angry Cows
+# http://www.usaco.org/index.php?page=viewproblem2&cpid=666 - Counting Haybales
+# https://dmoj.ca/problem/crci06p3 - Firefly
---
+++ readme.md
@@ -0,0 +1,30 @@
+# Clinicer Sukkasaippua koodikoulu
+
+Steps to get started.
+
+# Create an account at: https://dmoj.ca/
+# Create github account and fork the repo. Then clone it locally and push your changes to the master. We will go over your solutions in your own repo. If you need help with this hit me up.
+# Start with part 2.
+
+
+## Part 1: Learning to code by solving problems.
+
+There will be python modules with urls to DMOJ competitive programming problems. You're job is to solve those problems and then we will go through the solutions together.
+
+## Favela-GPT rules:
+ - Never ever include the actual problem in the question.
+
+## Allowed questions:
+ - "With python how to get 3rd element from list"
+ - "How to get all unique elements from a list"
+
+
+## Python environments, virtual environments, imports, dependency handling and packaging. 
+ - Will do in future.
+
+
+## Data-Engineering concepts.
+ - Future
+
+## Python and Fabric
+ - Future
